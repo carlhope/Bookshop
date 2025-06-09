d0aafebe3b82de3e9f2fe6f895e71a3bc4ae86a5
Carl hope <84421131+carlhope@users.noreply.github.com>
2025-06-09 15:03:31 +0100

    rename book controller

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Book;
 use Illuminate\Http\Request;
 
-class BooksController extends Controller
+class BookController extends Controller
 {
     public function index()
     {
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BooksController;
+use App\Http\Controllers\BookController;
 use App\Http\Controllers\CartController;
 
 Route::get('/', function () {
@@ -9,8 +9,8 @@ Route::get('/', function () {
 });
 
 
-Route::get('/books', [BooksController::class, 'index']);
-Route::get('/books/{id}', [BooksController::class, 'show'])->name('books.show');
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/{id}', [BookController::class, 'show'])->name('books.show');
 
 Route::post('/cart/add/{id}', [CartController::class, 'addToCart'])->name('cart.add');
 Route::get('/cart', [CartController::class, 'viewCart'])->name('cart.view');
