6c36faf318c543c8efc2bd774fc97d8ed15e61b5
Carl hope <84421131+carlhope@users.noreply.github.com>
2025-06-12 13:16:27 +0100

    change text in home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,5 @@
 
 @section('content')
     <h2 class="text-2xl font-semibold mb-4">Welcome to the Bookshop</h2>
-    <p class="text-lg">Discover a collection of books tailored to your interests.</p>
+    <p class="text-lg">Discover our collection of fiction books.</p>
 @endsection
